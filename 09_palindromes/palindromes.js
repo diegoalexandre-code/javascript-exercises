28fffb1e69bec468699306ee68f84b81b09f5911
Diego Alexandre <115668545+diegoalexandre-code@users.noreply.github.com>
2024-11-24 17:43:44 +0000

    Complete exercises 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const punctuation = [",", ".", " ", ":", ";", "!"];
+  const word = string
+    .toLowerCase()
+    .split("")
+    .filter((item) => !punctuation.includes(item));
+  const wordReversed = word.slice().reverse();
+  return word.every((value, index) => value === wordReversed[index]);
 };
 
 // Do not edit below this line

72246d425ba02a75810a0dabae3e645d822aaee3
Diego Alexandre <diegoalexandre_@live.com>
2024-11-28 20:12:40 -0300

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = array.map(item => item.title)
+    return titles
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;

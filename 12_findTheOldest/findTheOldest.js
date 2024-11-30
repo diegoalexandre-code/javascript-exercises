cc6b7e4d9916755cfb754173950cf6bfe5659c4d
Diego Alexandre <diegoalexandre_@live.com>
2024-11-30 18:12:52 -0300

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let person = array
+        .reduce((acc, item) => { 
+            if(Object.keys(acc).length === 0){
+                acc = item
+            }else {
+                let currentYear = new Date().getFullYear()
+                let ageAcc = (acc.yearOfDeath || currentYear) - acc.yearOfBirth
+                let ageItem = (item.yearOfDeath || currentYear) - item.yearOfBirth
 
+                if( ageAcc < ageItem ){
+                    acc = item
+                }
+            }
+            return acc
+            
+        } 
+        ,{})
+    return person        
 };
 
 // Do not edit below this line
